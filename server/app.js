cd014db8786321e0e76cbe4470b07fa9f45d7f5e
Tavo Cruz <madecruz2@gmail.com>
2023-09-20 22:10:49 -0700

    start connection to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const app = express();
 const PORT = 5000;
+const mongoose = require('mongoose')
+const { MongoUrl } = require('./keys')
 
-const customMiddleWare = (req,res,next) => {
-    console.log("middleware executed!!!")
-    next();
-}
-
-// app.use(customMiddleWare);
-
-app.get('/', customMiddleWare, (req, res) => {
-    console.log("home");
-    res.send("Hello World")
-})
-
+mongoose.connect(MongoUrl)
+    .then(() => console.log("connected to db successfully"))
+    .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
