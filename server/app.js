77d55c3a3a933aa369c156944648f78767e9e341
Tavo Cruz <madecruz2@gmail.com>
2023-09-20 22:18:03 -0700

    start User Schema model

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const PORT = 5000;
 const mongoose = require('mongoose')
 const { MongoUrl } = require('./keys')
 
+const User = require('./models/User')
+
 mongoose.connect(MongoUrl)
     .then(() => console.log("connected to db successfully"))
     .catch(err => console.log(err));
--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoUrl:"Your Own"
+    MongoUrl:"mongodb+srv://1tav0:Legendary1010@cluster0.pqz2ftk.mongodb.net/"
 }
