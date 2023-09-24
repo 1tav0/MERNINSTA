9ff8728c0d4fb2c2138e772a27162b613014f814
Tavo Cruz <madecruz2@gmail.com>
2023-09-23 23:57:23 -0700

    hashing the password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-
-router.get('/', (req, res) => {
-    res.send('hello')
-})
+const bcrypt = require('bcryptjs')
 
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
@@ -14,29 +11,31 @@ router.post('/signup', (req, res) => {
         return res.status(422).json({ error: "Please add all the fields" });
     }
 
-    User.findOne({ email: email })
-        .then((savedUser) => {
-            if (savedUser) {
-                res.status(422).json({ error: "User with the email already exits"})
-            }
-            const user = new User({
-                name,
-                email, 
-                password
-            })
+    bcrypt.hash(password, 12)
+        .then((hashedpassword) => {
+            User.findOne({ email: email })
+            .then((savedUser) => {
+                if (savedUser) {
+                    res.status(422).json({ error: "User with the email already exits"})
+                }
+                const user = new User({
+                    name,
+                    email, 
+                    password: hashedpassword
+                })
 
-            user.save()
-                .then((user) => {
-                    res.json({message: "User saved Successfully"})
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
+                user.save()
+                    .then((user) => {
+                        res.json({message: "User saved Successfully"})
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            })
         })
         .catch((error) => {
             console.log(error);
-        })
-        
+        })   
 })
 
 module.exports = router
