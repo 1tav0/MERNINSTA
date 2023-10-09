cb096101c8ab424e543240b406ba66361e6548a0
Tavo Cruz <madecruz2@gmail.com>
2023-10-09 16:09:08 -0700

    create profile page & home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import './Home.css';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home__wrapper">
+      <div className="card home__card">
+        <h5>Tavo</h5>
+        <div className="card-image">
+          <img className='home__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+        </div>
+        <div className="card-content">
+          <FavoriteBorderIcon />
+          <h6>likes</h6>
+          <h6>Title of post</h6>
+          <p>This is the post body</p>
+          <input type='text' placeholder='add a comment' />
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React from 'react';
+import './Profile.css';
+import Avatar from '@mui/material/Avatar';
+import SettingsIcon from '@mui/icons-material/Settings';
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile__wrapper'>
+      <div className="profile__wrapper__child">
+        <div className="profile__picture">
+          <Avatar
+            sx={{ width: 100, height: 100 }}
+            alt="title" src="https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg" />
+        </div>
+        <div className="profile__rightside">
+          <div className="profile__top">
+            <span>tavo__username</span>
+            <button>Edit Profile</button>
+            <SettingsIcon />
+          </div>
+          <div className="profile__middle">
+            <span>80 posts</span>
+            <span>376 followers</span>
+            <span>300 following</span>
+          </div>
+          <div className="profile__bottom">
+            <span>tavo@gmail.com</span>
+            <span>Software engineer</span>
+            <span>www.octavio-cruz.com</span>
+          </div>
+        </div>
+      </div>
+      <div className="profile__gallery">
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+          <img className='item__img' src='https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg' alt='photo' />
+      </div>
+    </div>
   )
 }
 
