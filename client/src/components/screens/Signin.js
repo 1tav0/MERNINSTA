03d67f8e6866677412cb3b0d68904482457b0dd1
Tavo Cruz <madecruz2@gmail.com>
2023-10-09 14:07:10 -0700

    Create signin & signup UI

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import './Signin.css';
+import { Link } from 'react-router-dom';
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='card__wrapper'>
+      <div className="card card__child input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder='email' />
+        <input type="text" placeholder='password' />
+        <button className="btn waves-effect waves-light #1e88e5 blue darken-1">Login
+        </button>
+        <h5>
+          <Link to="/signup">Don't have an account ?</Link>
+        </h5>
+      </div>
+      
+    </div>
   )
 }
 
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './Signup.css'
+import { Link } from 'react-router-dom'
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='card__wrapper'>
+      <div className="card card__child input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder='name' />
+        <input type="text" placeholder='email' />
+        <input type="text" placeholder='password' />
+        <button className="btn waves-effect waves-light #1e88e5 blue darken-1">Login
+        </button>
+        <h5>
+          <Link to="/signin">Already have an account ?</Link>
+        </h5>
+      </div>
+      
+    </div>
   )
 }
 
