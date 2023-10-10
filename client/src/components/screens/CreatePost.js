da1d0305f93e8cdd7330c24bc0ce733ad3ecd316
Tavo Cruz <madecruz2@gmail.com>
2023-10-10 11:46:59 -0700

    UI signup by posting data in front end

--- client/src/components/screens/CreatePost.js
+++ client/src/components/screens/CreatePost.js
@@ -6,13 +6,13 @@ const CreatePost = () => {
     <div className='card input-field wrapper'>
       <input type='text' placeholder='title' />
       <input type='text' placeholder='body' />
-      <div class="file-field input-field">
-        <div class="btn #1e88e5 blue darken-1">
+      <div className="file-field input-field">
+        <div className="btn #1e88e5 blue darken-1">
           <span>Upload Image</span>
           <input type="file" />
         </div>
-        <div class="file-path-wrapper">
-          <input class="file-path validate" type="text" />
+        <div className="file-path-wrapper">
+          <input className="file-path validate" type="text" />
         </div>
       </div>
       <button className="btn waves-effect waves-light #1e88e5 blue darken-1 submit__button">
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
 import { Link } from 'react-router-dom'
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const uploadData = async () => {
+    try {
+      const request = await fetch("/signup", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: "",
+          email: "",
+          password: ""
+        })
+      });
+      const response = await request.json();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className='card__wrapper'>
       <div className="card card__child input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder='name' />
-        <input type="text" placeholder='email' />
-        <input type="text" placeholder='password' />
-        <button className="btn waves-effect waves-light #1e88e5 blue darken-1">Login
+        <input
+          type="text"
+          placeholder='name'
+          value={name}
+          onChange={event => {
+            setName(event.target.value)
+          }}
+        />
+        <input
+          type="text"
+          placeholder='email'
+          value={email}
+          onChange={event => {
+            setEmail(event.target.value)
+          }}
+        />
+        <input
+          type="text"
+          placeholder='password'
+          value={password}
+          onChange={event => {
+            setPassword(event.target.value)
+          }}
+        />
+        <button className="btn waves-effect waves-light #1e88e5 blue darken-1 signup__button"
+          onClick={()=>uploadData()}
+        >SignUp
         </button>
         <h5>
           <Link to="/signin">Already have an account ?</Link>
